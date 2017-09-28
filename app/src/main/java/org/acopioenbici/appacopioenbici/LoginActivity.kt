4c33e1ff24fd8859047ba2ae309127d4daf318e5
Fabián Heredia Montiel <fabianherediajr@gmail.com>
2017-09-27 23:49:31 -0500

    Añadir firebase para login

--- app/src/main/java/org/acopioenbici/appacopioenbici/LoginActivity.kt
+++ app/src/main/java/org/acopioenbici/appacopioenbici/LoginActivity.kt
@@ -4,10 +4,24 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import com.twitter.sdk.android.core.*
 import com.twitter.sdk.android.core.identity.TwitterLoginButton
 import org.jetbrains.anko.longToast
 import org.jetbrains.anko.toast
+import com.google.firebase.auth.FirebaseUser
+import android.widget.Toast
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.TwitterAuthProvider
+import com.google.firebase.auth.AuthCredential
+import com.twitter.sdk.android.core.TwitterSession
+
+
+
+
 
 /**
  * A login screen that offers login via Twitter.
@@ -16,9 +30,11 @@ class LoginActivity : AppCompatActivity() {
     var sessionManager : SessionManager<TwitterSession>? = null
     var currentSession : TwitterSession?     = null
     var loginButton    : TwitterLoginButton? = null
+    var mAuth          : FirebaseAuth?       = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Twitter.initialize(this);
+        mAuth = FirebaseAuth.getInstance();
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -34,11 +50,7 @@ class LoginActivity : AppCompatActivity() {
         loginButton?.setCallback(object : Callback<TwitterSession>() {
             // Login Exitoso
             override fun success(result: Result<TwitterSession>) {
-                if (true) {
-                    launchMain()
-                } else {
-                    longToast("Usuario No Activo")
-                }
+                handleTwitterSession(result.data)
             }
 
             // Login Fallo
@@ -48,11 +60,34 @@ class LoginActivity : AppCompatActivity() {
         })
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth?.getCurrentUser()?.let { launchMain() }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         super.onActivityResult(requestCode, resultCode, data)
 
         // Pass the activity result to the login button.
         loginButton?.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun handleTwitterSession(session: TwitterSession) {
+        val credential = TwitterAuthProvider.getCredential(
+                session.authToken.token,
+                session.authToken.secret)
+
+        mAuth?.signInWithCredential(credential)?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        launchMain()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        longToast("Authentication failed.")
+                    }
+                }
     }
 
     fun launchMain() {
