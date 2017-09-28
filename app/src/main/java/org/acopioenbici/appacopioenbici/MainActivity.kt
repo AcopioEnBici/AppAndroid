b28b999ac62a9b1aafb5736d92ddbfd124954e26
Fabián Heredia Montiel <fabianherediajr@gmail.com>
2017-09-28 16:47:25 -0500

    Obtener primero datos de Firebase

--- app/src/main/java/org/acopioenbici/appacopioenbici/MainActivity.kt
+++ app/src/main/java/org/acopioenbici/appacopioenbici/MainActivity.kt
@@ -6,22 +6,33 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 import com.twitter.sdk.android.core.SessionManager
 import com.twitter.sdk.android.core.Twitter
 import com.twitter.sdk.android.core.TwitterCore
 import com.twitter.sdk.android.core.TwitterSession
+import org.jetbrains.anko.longToast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+
+
 
 class MainActivity : AppCompatActivity() {
 
     var currentSession : TwitterSession?                 = null
     var sessionManager : SessionManager<TwitterSession>? = null
-    var mAuth          : FirebaseAuth?                   = null
+    var user           : FirebaseUser?                   = null
+    var db             : FirebaseDatabase?               = null
+    var activo         : Boolean                         = false
 
     private var mTextMessage: TextView? = null
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_donations -> {
+                validateUser()
                 mTextMessage!!.setText(R.string.title_donations)
                 return@OnNavigationItemSelectedListener true
             }
@@ -40,14 +51,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Twitter.initialize(this);
-        mAuth = FirebaseAuth.getInstance();
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Twitter.initialize(this);
         sessionManager = TwitterCore.getInstance().getSessionManager()
         currentSession = sessionManager?.getActiveSession()
+
+        user = FirebaseAuth.getInstance()?.getCurrentUser()
+        db = FirebaseDatabase.getInstance()
+
+        // Checa si el voluntario esta activo
+        db?.getReference("/volunteers")?.
+                child(user?.uid)?.child("active")?.
+                addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        activo = snapshot.value as Boolean
+                        validateUser()
+                    }
+                    override fun onCancelled(error: DatabaseError?) {
+                        longToast(error.toString())
+                    }
+                })
+
+        // Obtiene donaciones
+        db?.getReference("donations")?.limitToFirst(10)?.
+                addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        mTextMessage?.setText(snapshot.value.toString())
+                    }
+
+                    override fun onCancelled(error: DatabaseError?) {
+                        longToast(error.toString())
+                    }
+                })
 
         mTextMessage = findViewById(R.id.message) as TextView
         val navigation = findViewById(R.id.navigation) as BottomNavigationView
@@ -56,15 +93,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        validateUser()
+    }
 
-        if (mAuth?.getCurrentUser() == null
-                || currentSession == null) {
-            signout()
-        }
+    fun validateUser() {
+        if (user == null || currentSession == null) signout()
     }
 
     fun signout() {
-        FirebaseAuth.getInstance().signOut();
+        FirebaseAuth.getInstance()?.signOut()
 
         currentSession?.id?.let {
             sessionManager?.clearSession(it)
