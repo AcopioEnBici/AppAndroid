29e09c4a0c127e9eb7913370997cc2b4be91b324
Fabián Heredia Montiel <fabianherediajr@gmail.com>
2017-09-27 22:30:47 -0500

    Ahora sí ya funciona el login

--- app/src/main/java/org/acopioenbici/appacopioenbici/LoginActivity.kt
+++ app/src/main/java/org/acopioenbici/appacopioenbici/LoginActivity.kt
@@ -3,35 +3,60 @@ package org.acopioenbici.appacopioenbici
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.util.Log
 import com.twitter.sdk.android.core.*
 import com.twitter.sdk.android.core.identity.TwitterLoginButton
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.toast
 
 /**
  * A login screen that offers login via Twitter.
  */
 class LoginActivity : AppCompatActivity() {
+    var currentSession = null as TwitterSession?
+    var loginButton    = null as TwitterLoginButton?
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Twitter.initialize(this);
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val loginButton = findViewById(R.id.login_button) as TwitterLoginButton?
+        currentSession = TwitterCore.getInstance().getSessionManager().getActiveSession() as TwitterSession?
+        loginButton    = findViewById(R.id.login_button) as TwitterLoginButton?
 
-        if(loginButton != null) {
-            loginButton.setCallback(object : Callback<TwitterSession>() {
-                override fun success(result: Result<TwitterSession>) {
-                    // Do something with result, which provides a TwitterSession for making API calls
+        // Check if Current Session
+        currentSession?.getUserName()?.let { longToast(it) }
+
+        // Twitter Login Logic
+        loginButton?.setCallback(object : Callback<TwitterSession>() {
+            // Login Exitoso
+            override fun success(result: Result<TwitterSession>) {
+                longToast(result.toString())
+                if (true) {
+                    launchMain()
+                } else {
+                    longToast("Usuario No Activo")
                 }
+            }
 
-                override fun failure(exception: TwitterException) {
-                    // Do something on failure
-                }
-            })
-        }
-        else {
+            // Login Fallo
+            override fun failure(exception: TwitterException) {
+                longToast(exception.toString())
+            }
+        })
+    }
 
-        }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Pass the activity result to the login button.
+        loginButton?.onActivityResult(requestCode, resultCode, data)
+    }
+
+    fun launchMain() {
+        val i = Intent(applicationContext, MainActivity::class.java)
+        startActivity(i);
     }
 }
 
